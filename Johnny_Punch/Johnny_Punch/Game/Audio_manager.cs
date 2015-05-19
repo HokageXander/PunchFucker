473159f0da0462be90a83a02a7f161a5ad88f018
JonatanSvensson <svenssonsnabel@gmail.com>
2015-05-19 15:09:27 +0200

    ..

--- Johnny_Punch/Johnny_Punch/Game/Audio_manager.cs
+++ Johnny_Punch/Johnny_Punch/Game/Audio_manager.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Johnny_Punch
 {
-    class AudioManager
+    public static class AudioManager
     {
+        public static Song Bla { get; private set; }
 
+        public static void LoadContent(ContentManager Content)
+        {
+            Bla = Content.Load<Song>(@"Audio\Bla");    
+        }
     }
 }
